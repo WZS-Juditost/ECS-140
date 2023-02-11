83f2fb7b2497000e8aeeebc43d6903ed2807c81d
WZS-Juditost <wzswang@ucdavis.edu>
2023-02-11 15:34:31 -0800

    1

--- hw2_haskell/q4_square_a_b.hs
+++ hw2_haskell/q4_square_a_b.hs
@@ -9,3 +9,7 @@ square x = x * x
 
 result :: Integer
 result = square (a + b)
+
+main :: IO ()
+main = do
+    putStrLn $ "The result of square (" ++ show a  ++ " + " ++ show b ++ ") is " ++ show result
